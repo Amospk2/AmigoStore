54c17ed69f123eb98b089f3648b6c6c2cd9efc11
deadAdrian <0adrianvidal@gmail.com>
2023-08-22 18:28:38 -0300

    feat: chakra provider com tema custom

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,24 @@
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
+// import * as theme from 'config/chakra.config'
+
+import theme from 'web/config/theme'
+
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
-
 import './index.css'
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
+      <ColorModeScript />
+      <ChakraProvider theme={theme}>
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </ChakraProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
