1ceed75e66319ea2360ca838030de8d5d0c1dfd2
deadAdrian <0adrianvidal@gmail.com>
2023-08-22 18:28:46 -0300

    wip: homepage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,16 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+
+import Banner from 'src/components/Banner/Banner'
+import GroupProducts from 'src/components/GroupProducts/GroupProducts'
 import './homePage.scss'
+
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <Banner />
+      <GroupProducts />
     </>
   )
 }
